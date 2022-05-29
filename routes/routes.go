c40673576981d2b6a607a817c620704be57e8b99
Jader Campos <jader.campos@gmail.com>
2022-05-29 03:21:00 -0300

    Add middleware with contenttype

--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"net/http"
 	"rest-api-go/controllers"
+	"rest-api-go/middleware"
 
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/Personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/Personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
