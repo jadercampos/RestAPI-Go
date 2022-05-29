d189f8bfb9bbe7bb11fb83c505ea4eb41718cf12
Jader Campos <jader.campos@gmail.com>
2022-05-29 03:07:40 -0300

    CRUD GORM Delete

--- routes/routes.go
+++ routes/routes.go
@@ -14,5 +14,6 @@ func HandleRequest() {
 	r.HandleFunc("/Personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/Personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/Personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/Personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
